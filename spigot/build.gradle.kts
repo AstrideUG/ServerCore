f8b298eae7ebf35b09a8b4237b5b366b69a9ae4f
LartyHD <LartyHD@minewar.de>
2019-01-12 18:56:38 +0100

    first commit

---
+++ spigot/build.gradle.kts
@@ -0,0 +1,21 @@
+/*
+ * © Copyright - Lars Artmann aka. LartyHD 2018.
+ */
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.net.URI
+import org.jetbrains.kotlin.resolve.calls.inference.CapturedType
+
+plugins {
+    java
+    kotlin("jvm")
+}
+
+dependencies {
+    compileKotlin()
+    compileTest()
+    compile(project(":APIs:Events:Events-Api"))
+}
+
+fun DependencyHandlerScope.compileKotlin() = (extra["compileKotlin"] as Function1<DependencyHandlerScope, *>)(this)
+fun DependencyHandlerScope.compileTest() = (extra["compileTest"] as Function1<DependencyHandlerScope, *>)(this)

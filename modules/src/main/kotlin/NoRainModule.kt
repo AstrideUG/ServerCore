3d1e6679b60d2e628664f9b99b08994ab08fbcdd
LartyHD <lartyhd@gmail.com>
2019-04-01 14:15:49 +0200

    Update to Darkness-Spigot-1.0

--- modules/src/main/kotlin/NoRainModule.kt
+++ modules/src/main/kotlin/NoRainModule.kt
@@ -1,10 +1,12 @@
+import com.google.gson.JsonArray
 import de.astride.minecraft.servercore.spigot.ServerCoreSpigotPlugin
 import net.darkdevelopers.darkbedrock.darkness.general.configs.ConfigData
-import net.darkdevelopers.darkbedrock.darkness.general.configs.gson.GsonService
+import net.darkdevelopers.darkbedrock.darkness.general.configs.gson.GsonService.loadAs
+import net.darkdevelopers.darkbedrock.darkness.general.functions.asString
 import net.darkdevelopers.darkbedrock.darkness.general.modules.Module
 import net.darkdevelopers.darkbedrock.darkness.general.modules.ModuleDescription
+import net.darkdevelopers.darkbedrock.darkness.spigot.functions.cancel
 import net.darkdevelopers.darkbedrock.darkness.spigot.listener.Listener
-import org.bukkit.Bukkit
 import org.bukkit.World
 import org.bukkit.event.EventHandler
 import org.bukkit.event.weather.WeatherChangeEvent
@@ -14,38 +16,30 @@ import org.bukkit.plugin.java.JavaPlugin
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 05.07.2018 11:17.
- * Last edit 02.12.2018
+ * Last edit 01.04.2019
  */
 class NoRainModule : Module, Listener(JavaPlugin.getPlugin(ServerCoreSpigotPlugin::class.java)) {
 
     override val description: ModuleDescription =
-        ModuleDescription(javaClass.canonicalName, "1.3", "Lars Artmann | LartyHD", "This modules block rain")
+        ModuleDescription(javaClass.canonicalName, "1.3.2", "Lars Artmann | LartyHD", "This modules block rain")
 
-    private val worlds by lazy {
-        try {
-            GsonService.loadAsJsonArray(ConfigData(description.folder, "worlds.json")).mapNotNull {
-                try {
-                    Bukkit.getWorld(it.asJsonPrimitive.asString)
-                } catch (ex: Exception) {
-                    null
-                }
-            }
-        } catch (ex: ClassCastException) {
-            emptyList<World>()
-        }
+    private val worlds: List<String> by lazy {
+        val configData = ConfigData(description.folder, "worlds.json")
+        val jsonArray = loadAs(configData) ?: JsonArray()
+        jsonArray.mapNotNull { it.asString() }
     }
 
     @EventHandler
     fun onWeatherChangeEvent(event: WeatherChangeEvent) {
-        if (worlds.isNotEmpty() || worlds.contains(event.world)) cancel(event, event.toWeatherState())
+        if (worlds.isEmpty() || event.world.name in worlds) event.cancel(event.toWeatherState())
     }
 
     @EventHandler
-    fun onWorldLoadEvent(event: WorldLoadEvent) = clearWeather(event.world)
+    fun onWorldLoadEvent(event: WorldLoadEvent) = event.world.clearWeather()
 
-    private fun clearWeather(world: World) {
-        world.setStorm(false)
-        world.isThundering = false
+    private fun World.clearWeather() {
+        setStorm(false)
+        isThundering = false
     }
 
 }

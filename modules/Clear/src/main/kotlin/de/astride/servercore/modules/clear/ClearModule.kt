567370e8b604db5dda10dc0b1a0205ed5a960316
LartyHD <lartyhd@gmail.com>
2019-05-13 05:55:45 +0200

    Fixed ClearModule imports

--- modules/Clear/src/main/kotlin/de/astride/servercore/modules/clear/ClearModule.kt
+++ modules/Clear/src/main/kotlin/de/astride/servercore/modules/clear/ClearModule.kt
@@ -1,21 +1,19 @@
 package de.astride.servercore.modules.clear
 
 import de.astride.minecraft.servercore.spigot.ServerCoreSpigotPlugin
+import net.darkdevelopers.darkbedrock.darkness.spigot.commands.SimplePlayerCommandModule
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
 /**
- * Created by DevSnox on 12.02.18
- * Copyright (c) 2018 DevSnox
- * GitHub: https://github.com/DevSnox
- * Web: http://devsnox.me
- * Mail: me.devsnox@gmail.com
- * Discord: DevSnox#4884 | Skype: live:chaos3729
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 13.05.2019 05:55.
+ * Current Version: 1.0 (13.05.2019 - 13.05.2019)
  */
 class ClearModule : SimplePlayerCommandModule("clear") {
 
-    override val command: () -> SimplePlayerCommandModule.PlayerCommand = {
+    override val command: () -> PlayerCommand = {
         PlayerCommand(JavaPlugin.getPlugin(ServerCoreSpigotPlugin::class.java))
     }
 

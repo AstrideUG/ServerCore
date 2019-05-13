3b638ec1f8abe07fa4af8bbbbc73ea65d546d744
LartyHD <lartyhd@gmail.com>
2019-05-13 04:26:10 +0200

    Added javaPlugin to ServerCoreSpigotPlugin.Companion

--- spigot/src/main/kotlin/de/astride/minecraft/servercore/spigot/ServerCoreSpigotPlugin.kt
+++ spigot/src/main/kotlin/de/astride/minecraft/servercore/spigot/ServerCoreSpigotPlugin.kt
@@ -6,12 +6,16 @@ import net.darkdevelopers.darkbedrock.darkness.spigot.plugin.DarkPlugin
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 12.01.2019 19:17.
- * Current Version: 1.0 (12.01.2019 - 12.01.2019)
+ * Current Version: 1.0 (12.01.2019 - 13.05.2019)
  */
 class ServerCoreSpigotPlugin : DarkPlugin() {
 
     override fun onEnable() = onEnable {
         ServerCore(dataFolder)
+    }
+
+    companion object {
+        val javaPlugin: ServerCoreSpigotPlugin = getPlugin(ServerCoreSpigotPlugin::class.java)
     }
 
 }

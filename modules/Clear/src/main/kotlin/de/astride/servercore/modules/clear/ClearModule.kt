5338e64d8d985564da9b6d9e59e488b00ee5a0a4
DevSnox <me.devsnox@gmail.com>
2019-05-13 04:45:12 +0200

    Seperated Clear into own module.

--- modules/Clear/src/main/kotlin/de/astride/servercore/modules/clear/ClearModule.kt
+++ modules/Clear/src/main/kotlin/de/astride/servercore/modules/clear/ClearModule.kt
@@ -1,5 +1,6 @@
+package de.astride.servercore.modules.clear
+
 import de.astride.minecraft.servercore.spigot.ServerCoreSpigotPlugin
-import net.darkdevelopers.darkbedrock.darkness.spigot.commands.SimplePlayerCommandModule
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin

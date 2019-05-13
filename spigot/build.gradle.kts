505e00ffc45cfb24f3e3651920337c99d48c7944
LartyHD <lartyhd@gmail.com>
2019-05-13 02:03:06 +0200

    Fixed gradle build

--- spigot/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-/*
- * © Copyright - Lars Artmann aka. LartyHD 2018.
- */
-
-dependencies {
-    "jarLibs"(project(":ServerCore-Common"))
-    compileOnly("org.spigotmc", "spigot-api", extra["versions.spigot"].toString())
-}

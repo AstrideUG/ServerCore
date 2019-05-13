5d28317f693276c2d3b7bf580be0adc3fb86f961
LartyHD <lartyhd@gmail.com>
2019-05-13 03:20:41 +0200

    Changed ScoreboardModule to JarModule

--- modules/ScoreBoard/src/main/kotlin/de/astride/servercore/modules/scoreboard/ScoreboardModule.kt
+++ modules/ScoreBoard/src/main/kotlin/de/astride/servercore/modules/scoreboard/ScoreboardModule.kt
@@ -2,8 +2,8 @@
  * © Copyright - Lars Artmann aka. LartyHD 2019.
  */
 
-import com.google.gson.JsonArray
-import com.google.gson.JsonPrimitive
+package de.astride.servercore.modules.scoreboard
+
 import de.astride.minecraft.servercore.spigot.ServerCoreSpigotPlugin
 import net.darkdevelopers.darkbedrock.darkness.general.configs.ConfigData
 import net.darkdevelopers.darkbedrock.darkness.general.configs.gson.GsonConfig
@@ -11,11 +11,9 @@ import net.darkdevelopers.darkbedrock.darkness.general.configs.gson.GsonService
 import net.darkdevelopers.darkbedrock.darkness.general.modules.Module
 import net.darkdevelopers.darkbedrock.darkness.general.modules.ModuleDescription
 import net.darkdevelopers.darkbedrock.darkness.spigot.events.PlayerDisconnectEvent
-import net.darkdevelopers.darkbedrock.darkness.spigot.functions.JsonObject
 import net.darkdevelopers.darkbedrock.darkness.spigot.functions.events.listen
 import net.darkdevelopers.darkbedrock.darkness.spigot.functions.events.unregister
 import net.darkdevelopers.darkbedrock.darkness.spigot.functions.sendScoreBoard
-import net.darkdevelopers.darkbedrock.darkness.spigot.functions.toJsonElement
 import net.darkdevelopers.darkbedrock.darkness.spigot.manager.game.EventsTemplate
 import net.darkdevelopers.darkbedrock.darkness.spigot.messages.SpigotGsonMessages
 import net.darkdevelopers.darkbedrock.darkness.spigot.utils.Utils.players
@@ -23,6 +21,7 @@ import org.bukkit.ChatColor.*
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
 
 /**
  * @author Lars Artmann | LartyHD
@@ -30,6 +29,8 @@ import org.bukkit.plugin.java.JavaPlugin
  * Current Version: 1.1.0 (28.12.2018 - 13.05.2019)
  */
 class ScoreboardModule : Module, EventsTemplate() {
+
+    private val messagesKey: String = "messages.json"
 
     override val description: ModuleDescription = ModuleDescription(
         javaClass.canonicalName,
@@ -43,34 +44,21 @@ class ScoreboardModule : Module, EventsTemplate() {
 
     override fun start() {
 
-        val configData = ConfigData(description.folder, "config.json")
+        val configData = ConfigData(description.folder, messagesKey)
         @Suppress("DEPRECATION")
         val spigotGsonMessages = SpigotGsonMessages(GsonConfig(configData).load())
         val availableMessages = spigotGsonMessages.availableMessages
+
+        if (availableMessages.isEmpty()) {
+            val output = javaClass.getResourceAsStream("default${File.separator}$messagesKey").reader().readText()
+            GsonService.save(configData, output)
+        }
 
         displayName = availableMessages["display-name"]?.joinToString("")
             ?: "$AQUA${BOLD}CraftPlugin$WHITE$BOLD.$AQUA${BOLD}net"
         entries = availableMessages["entries"]?.mapNotNull { it }?.toSet()
             ?: setOf("", "powered by $AQUA${BOLD}CraftPlugin$WHITE$BOLD.$AQUA${BOLD}net")
 
-        if (availableMessages.isEmpty()) {
-            val jsonElement = JsonObject(
-                mapOf(
-                    "Messages" to JsonObject(
-                        mapOf(
-                            "language" to JsonPrimitive("de_DE"),
-                            "languages" to JsonObject(
-                                mapOf(
-                                    "display-name" to JsonPrimitive(displayName),
-                                    "entries" to (entries.toJsonElement() ?: JsonArray())
-                                )
-                            )
-                        )
-                    )
-                )
-            )
-            GsonService.save(configData, jsonElement)
-        }
 
         val plugin: ServerCoreSpigotPlugin = JavaPlugin.getPlugin(ServerCoreSpigotPlugin::class.java)
         registerListener(plugin)

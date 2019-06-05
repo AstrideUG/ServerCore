0b0c4a0d4cf0056b4dcb21ff30d3d66962eecb36
LartyHD <lartyhd@gmail.com>
2019-06-05 19:00:58 +0200

    Updated OnlyProxyJoin to Darkness-Spigot-4.0.0

--- modules/OnlyProxyJoin/src/main/kotlin/de/astride/servercore/modules/onlyproxyjoin/OnlyProxyJoin.kt
+++ modules/OnlyProxyJoin/src/main/kotlin/de/astride/servercore/modules/onlyproxyjoin/OnlyProxyJoin.kt
@@ -6,10 +6,10 @@ package de.astride.servercore.modules.onlyproxyjoin
 
 import com.google.gson.JsonObject
 import de.astride.minecraft.servercore.spigot.ServerCoreSpigotPlugin
-import net.darkdevelopers.darkbedrock.darkness.general.configs.ConfigData
 import net.darkdevelopers.darkbedrock.darkness.general.configs.default
 import net.darkdevelopers.darkbedrock.darkness.general.configs.getValue
 import net.darkdevelopers.darkbedrock.darkness.general.functions.load
+import net.darkdevelopers.darkbedrock.darkness.general.functions.toConfigData
 import net.darkdevelopers.darkbedrock.darkness.general.functions.toMap
 import net.darkdevelopers.darkbedrock.darkness.general.modules.Module
 import net.darkdevelopers.darkbedrock.darkness.general.modules.ModuleDescription
@@ -22,19 +22,19 @@ import org.bukkit.event.player.PlayerLoginEvent
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 02.06.2019 23:31.
- * Last edit 02.06.2019
+ * Last edit 05.06.2019
  */
 class OnlyProxyJoin : Module, EventsTemplate() {
 
     override val description: ModuleDescription = ModuleDescription(
         javaClass.simpleName,
-        "1.0.0",
+        "1.0.1",
         "Lars Artmann | LartyHD",
         "This module asks if the host address is valid"
     )
 
     override fun start() {
-        val config = Config(ConfigData(description.folder, "config.json").load<JsonObject>().toMap())
+        val config = Config(description.folder.toConfigData("config").load<JsonObject>().toMap())
         listen<PlayerLoginEvent>(ServerCoreSpigotPlugin.javaPlugin, priority = EventPriority.HIGHEST) { event ->
             if (config.proxyIps.none { it == event.realAddress.hostAddress })
                 event.disallow(PlayerLoginEvent.Result.KICK_OTHER, config.kickMessage)

e9d817f454b537f9e5cc9f509a0115f5f05cd24d
LartyHD <lartyhd@gmail.com>
2019-06-21 23:30:07 +0200

    Added Velocity API components to ServerCore-Velocity

--- velocity/src/main/kotlin/de/astride/minecraft/servercore/velocity/ServerCoreVelocityPlugin.kt
+++ velocity/src/main/kotlin/de/astride/minecraft/servercore/velocity/ServerCoreVelocityPlugin.kt
@@ -8,9 +8,12 @@ import com.google.inject.Inject
 import com.velocitypowered.api.event.Subscribe
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent
 import com.velocitypowered.api.plugin.Plugin
+import com.velocitypowered.api.plugin.PluginDescription
 import com.velocitypowered.api.plugin.annotation.DataDirectory
+import com.velocitypowered.api.proxy.ProxyServer
 import de.astride.minecraft.servercore.common.ServerCore
 import java.nio.file.Path
+import java.util.logging.Logger
 
 /**
  * Created on 21.06.2019 22:55.
@@ -22,11 +25,30 @@ import java.nio.file.Path
     version = "@version@",
     authors = ["Lars Artmann | LartyHD"]
 )
-class ServerCoreVelocityPlugin @Inject private constructor(@DataDirectory private val path: Path) {
+class ServerCoreVelocityPlugin @Inject private constructor(
+    server: ProxyServer,
+    logger: Logger,
+    @DataDirectory path: Path,
+    description: PluginDescription
+) {
+
+    init {
+        Companion.server = server
+        Companion.logger = logger
+        Companion.path = path
+        Companion.description = description
+    }
 
     @Subscribe
     fun on(event: ProxyInitializeEvent) {
         ServerCore(path.toFile())
+    }
+
+    companion object {
+        lateinit var server: ProxyServer
+        lateinit var logger: Logger
+        lateinit var path: Path
+        lateinit var description: PluginDescription
     }
 
 }
